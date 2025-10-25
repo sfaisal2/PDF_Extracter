8445c0baaae25e445a5c63662e052312ca23f97f
sfaisal2 <sarahfaisal126@gmail.com>
2025-10-25 15:32:43 -0500

    libraries and samples

--- library1.py
+++ library1.py
@@ -1,1 +1,28 @@
+import pymupdf as fitz
+import pymupdf4llm
+from pypdf import PdfReader
+import pdfplumber
 
+doc = fitz.open('sample.pdf')
+text = ""
+for page in doc:
+    text += page.get_text()
+print(text)
+
+extractors = [
+    "pymupdf",
+    "pymupdf4llm"
+    "pypdf",
+    "pdfplumber"
+]
+
+samples = [
+    "sample.pdf",
+    "sample2.pdf",
+    "sample3.pdf"
+]
+
+def extraction():
+    
+
+def main():
